32387fe7e1a5a8e01e19d5a0d70cbc1205ce3bc9
Lewis Goddard <lewisgoddard@eustasy.org>
2017-11-14 20:01:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+function lifespan(dob, lifespan = 100) {
+	// dob      = Date of Birth
+	// lifespan = years of expected lifespan
+	//            defaults to 100 years
+
+	var area          = document.getElementById('js--lifespan--area')
+	var text          = document.getElementById('js--lifespan--text')
+	var html          = ''
+	var current_date  = new Date()
+	dob               = new Date(dob)
+
+	var end_of_life   = new Date(dob)
+	end_of_life.setFullYear(end_of_life.getFullYear() + lifespan)
+
+	var days_lifespan = Math.round(
+		( end_of_life.getTime() - dob.getTime() )
+		/
+		( 1000 * 60 * 60 * 24 )
+	)
+
+	var days_lived    = Math.round(
+		// Get time stamps in milliseconds
+		( current_date.getTime() - dob.getTime() )
+		/
+		// Milliseconds * Second * Minutes * Hours
+		( 1000 * 60 * 60 * 24 )
+	)
+
+	var days_unlived  = ( days_lifespan - days_lived )
+
+	console.log('Date of Birth: '    + dob)
+	console.log('End of Life: '      + end_of_life)
+	console.log('Days in Lifespan: ' + days_lifespan)
+	console.log('Days Lived: '       + days_lived)
+	console.log('Days Unlived: '     + days_unlived)
+
+	html += '<div class="lifespan--day lifespan--day--lived"></div>'.repeat(days_lived)
+	html += '<div class="lifespan--day lifespan--day--unlived"></div>'.repeat(days_unlived)
+
+	area.innerHTML = html
+
+	text.innerHTML = '<p>Estimated days in lifespan: '   + days_lifespan.toLocaleString() +
+	                 '&emsp; Days lived: '               + days_lived.toLocaleString()    +
+	                 '&emsp; Estimated days remaining: ' + days_unlived.toLocaleString()  +
+	                 '&emsp; Completion: ' +
+	                 ( ( days_lived / days_lifespan ) * 100 ).toLocaleString() + ' %' +
+	                 '</p>'
+
+}
